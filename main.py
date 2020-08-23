6e0c3096d1f279098eb32d7033f456b9443ddeda
Kevin <kevingconyers@gmail.com>
2020-08-23 12:33:59 -0500

    Blocking issue fixed

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 import sys
 import re
 import requests
+import traceback
+from lxml.html import fromstring
+from itertools import cycle
 
 def validateQueury():
     if len(sys.argv) < 2:
@@ -19,13 +22,26 @@ def validateQueury():
     query = "+".join(sys.argv[1:]) #return query with http request seperator
     return query
 
-def makeRequest(query):
+def initProxies():
+    url = 'https://free-proxy-list.net/'
+    response = requests.get(url)
+    parser = fromstring(response.text)
+    proxies = set()
+    for i in parser.xpath('//tbody/tr')[:10]:
+        if i.xpath('.//td[7][contains(text(),"yes")]'):
+            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+            proxies.add(proxy)
+
+    return proxies
+
+def makeAmazonRequest(query, proxy):
     # The Search url is constant for amazon's results page
     # Simply appeneding the queury from earlier works
 
-    search_url = "https://www.amazon.com/s?k="
-    url = search_url + query
-    
+    search_url = "https://www.amazon.com/s?k=<q>&ref=nb_sb_noss_2"
+    url = re.sub("\<q\>", query, search_url)
+    print(url)
+
     #Found these header's via googling
     headers = { 
         'dnt': '1',
@@ -40,20 +56,45 @@ def makeRequest(query):
         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     }
 
-    r = requests.get(url, headers)
-
-    if r.status_code > 500:
-        if "To discuss automated access to Amazon data please contact" in r.text:
-            print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
+    try:
+        r = requests.get(url,proxies={"http": proxy, "https": proxy},headers=headers)
+        if r.status_code > 500:
+            print("Proxy %s failed"%proxy)
+            if "To discuss automated access to Amazon data please contact" in r.text:
+                print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
+            else:
+                print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
+                return None
+        elif r.status_code >= 200 and r.status_code <= 299:
+            print(r.status_code)
+            return r # Short circuit as soon as a good request is recieved
         else:
-            print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
+            print("Unhandled status code")
             return None
+            
+    except Exception as e:
+        print(e)
+        #Most free proxies will often get connection errors. You will have retry the entire request using another proxy to work. 
+        #We will just skip retries as its beyond the scope of this tutorial and we are only downloading a single url 
+        print("Skipping. Connnection error")
 
+    
 
 
 def main():
     q = validateQueury()
-    makeRequest(q)
+
+    proxies = initProxies()
+    proxy_pool = cycle(proxies)
+
+    response = None
+    for i in range(1,11):
+        #Get a proxy from the pool
+        proxy = next(proxy_pool)
+        print("Request #%d"%i)
+        response = makeAmazonRequest(q, proxy)
+        if response:
+            break
 
 
 main()
